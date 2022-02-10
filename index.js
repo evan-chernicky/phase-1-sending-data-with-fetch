2acba78fdec6e33833290beacd2e8385b181629d
Evan Chernicky <echernicky@gmail.com>
2022-02-09 19:52:19 -0500

    commit tree e1d952477709d47d6c8f02fe1765cc46b0d3fedc

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData (inputName, inputEmail) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+        },
+        body: JSON.stringify({
+            name: inputName,
+            email: inputEmail
+        })
+    })
+    .then(response => response.json())
+    .then(obj => {
+        document.body.innerHTML = obj[ "id" ]
+
+    })
+    .catch(function (error) {
+        document.body.innerHTML = 'Unauthorized Access'
+    })
+}
+
+submitData('Evan', 'echernicky@gmail.com')
